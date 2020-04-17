e333a65598bebeeb392150ed07ed732f4a233565
warumNicht <andrey_st_minev@yahoo.com>
2020-04-17 19:03:49 +0300

    install axios

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 export class Login extends React.PureComponent<any, any> {
   constructor(props: any) {
