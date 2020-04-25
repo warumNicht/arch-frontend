003dcb94e16102a852578910bc37c64fda6cbf16
warumNicht <andrey_st_minev@yahoo.com>
2020-04-25 11:44:52 +0300

    actions types

---
+++ src/redux/actions/ActionTypes.ts
@@ -0,0 +1,12 @@
+const TokenActions = {
+    SET_TOKEN: 'SET_TOKEN',
+    GET_TOKEN: 'GET_TOKEN'
+}
+
+const UserActions = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+    GET_CURRENT_USER: 'GET_CURRENT_USER'
+}
+
+export default TokenActions;
+export { UserActions };
