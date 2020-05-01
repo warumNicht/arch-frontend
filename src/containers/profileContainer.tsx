b5cbafce8d984115a838a05205eae71ec5cec52f
warumNicht <andrey_st_minev@yahoo.com>
2020-05-01 11:27:21 +0300

    decode token dependency

--- src/containers/profileContainer.tsx
+++ src/containers/profileContainer.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import ArchitectureAppStore from "../redux/interfaces/ArchitectureAppStore";
+var jwtDecode = require('jwt-decode');
 
 class ProfileContainer extends React.PureComponent<any, any> {
+
+  decode(){
+    if(this.props.token){
+      console.log(this.props.token)
+      var decoded = jwtDecode(this.props.token);
+      console.log(decoded);
+    }
+  
+  }
   render() {
     return (
       <div>
         <p>Logged in user:</p>
         <div>{this.props.token ? this.props.token : "Not logged in"}</div>
+        <button onClick={()=>{this.decode()}}>Decode</button>
       </div>
     );
   }
