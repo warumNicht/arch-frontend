7bb1e79afc1422ffbe730140dead3efa6678a3fd
Andrey Minev <andrey_st_minev@yahoo.com>
2020-05-06 15:35:55 +0300

    return needed attributes

--- src/util/api.ts
+++ src/util/api.ts
@@ -14,8 +14,13 @@ api.interceptors.response.use(function (response: AxiosResponse) {
   }, function (error: any) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    const res= error.response ? error.response : error.message;
-    return Promise.reject(res);
+
+    const errorFromServer = error.response ? {
+        status: error.response.status,
+        data: error.response.data      
+    } : error.message;
+    
+    return Promise.reject(errorFromServer);
   })
 
 export default api;
