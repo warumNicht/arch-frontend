a50ddd2537e1ee2eae8f5565043515c5accdba04
Andrey Minev <andrey_st_minev@yahoo.com>
2020-05-06 14:29:29 +0300

    simple solution

--- src/util/api.ts
+++ src/util/api.ts
@@ -12,25 +12,10 @@ api.interceptors.response.use(function (response: AxiosResponse) {
     return response;
 
   }, function (error: any) {
-
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    console.log(error.message, 'First');
-    (error as any).originalMessage = error.message;
-    Object.defineProperty(error, "message", { get: function () {
-        console.log(error.response, 'From object');
-        if (!error.response) {
-            return (error as any).originalMessage;
-        }
-        const message = {
-            data: error.response?.data,
-            status: error.response?.status
-        }
-        return message;
-    }});
-
-    console.log(error.message, 'Before promise');
-    return Promise.reject(error.message);
+    const res= error.response ? error.response : error.message;
+    return Promise.reject(res);
   })
 
 export default api;
