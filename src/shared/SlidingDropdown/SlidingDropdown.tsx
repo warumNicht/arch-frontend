26ce9cb53268bbc4dfc27171784c6eaf2db4ee41
Andrey Minev <andrey_st_minev@yahoo.com>
2020-05-30 13:42:11 +0300

    componentWillReceiveProps

--- src/shared/SlidingDropdown/SlidingDropdown.tsx
+++ src/shared/SlidingDropdown/SlidingDropdown.tsx
@@ -5,14 +5,21 @@ class SlidingDropdown extends React.PureComponent<any,any> {
         super(props)
         this.state={
             selectedIndex: 0,
-            data: this.props.data
+            data: props.data
         }
+    }
+
+    componentWillReceiveProps(props:any){
+        this.setState({
+            selectedIndex: 0,
+            data: props.data
+        })
     }
 
     renderList() :any{
         if(this.props.mapData){
             console.log(this.props.mapData)
-            return this.props.data.map((d:any, index:number)=>{
+            return this.state.data.map((d:any, index:number)=>{
                 return <li key={index} onClick={()=>{console.log(index)}}>
                     {this.props.mapData(d)}
                 </li>;
@@ -21,6 +28,8 @@ class SlidingDropdown extends React.PureComponent<any,any> {
     }
 
     render() {
+        console.log(this.props.data)
+        console.log(2)
         return (
             <div>
                 <div className='selected-item'>
