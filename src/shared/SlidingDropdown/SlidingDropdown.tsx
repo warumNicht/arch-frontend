fbd90c2f0d15bcfaf88cfe84d13f87708134958e
Andrey Minev <andrey_st_minev@yahoo.com>
2020-05-30 13:57:46 +0300

    selected item background

--- src/shared/SlidingDropdown/SlidingDropdown.tsx
+++ src/shared/SlidingDropdown/SlidingDropdown.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import './SlidingDropdown.css';
 
-class SlidingDropdown extends React.PureComponent<any,any> {
-    constructor(props:any){
+class SlidingDropdown extends React.PureComponent<any, any> {
+    constructor(props: any) {
         super(props)
-        this.state={
+        this.state = {
             selectedIndex: 0,
             data: props.data
         }
+        this.setSelectedIndex = this.setSelectedIndex.bind(this)
     }
 
-    componentWillReceiveProps(props:any){
+    componentWillReceiveProps(props: any) {
         this.setState({
             selectedIndex: 0,
             data: props.data
         })
     }
 
-    renderList() :any{
-        if(this.props.mapData){
+    renderSelectedItem(): any {
+        if (this.props.data.length > 0) {
             console.log(this.props.mapData)
-            return this.state.data.map((d:any, index:number)=>{
-                return <li key={index} onClick={()=>{console.log(index)}}>
+
+            return this.props.mapData(this.state.data[this.state.selectedIndex]);
+
+        }
+        return 'Title'
+    }
+
+    setSelectedIndex(index: number){
+        this.setState({
+            selectedIndex: index
+        })
+    }
+
+    renderList(): any {
+        if (this.props.mapData) {
+            console.log(this.props.mapData)
+            return this.state.data.map((d: any, index: number) => {
+                return <li key={index} onClick={() => { this.setSelectedIndex(index)}}
+                className={this.state.selectedIndex===index ? 'selected-item' : ''}>
                     {this.props.mapData(d)}
                 </li>;
             })
@@ -32,8 +51,8 @@ class SlidingDropdown extends React.PureComponent<any,any> {
         console.log(2)
         return (
             <div>
-                <div className='selected-item'>
-                    Title
+                <div className='displayed-item'>
+                    {this.renderSelectedItem()}
                 </div>
                 <ul className='dropdown-content'>
                     {this.renderList()}
