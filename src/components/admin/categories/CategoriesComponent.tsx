6914ca1b7fcb51759ade183d48fa626149f71264
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-18 12:20:11 +0300

    category create component

--- src/components/admin/categories/CategoriesComponent.tsx
+++ src/components/admin/categories/CategoriesComponent.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Switch, Route, Redirect, RouteComponentProps } from "react-router-dom";
+import { getLangPrefix } from "../../../util/LangPrefixUtil";
+import CategoryCreate from "./create/CategoryCreate";
 
 
 class CategoriesComponent extends React.PureComponent<any> {
@@ -8,6 +11,10 @@ class CategoriesComponent extends React.PureComponent<any> {
             <div>
                 <h1>CategoriesComponent</h1>
             
+                <Switch>
+                    <Route path={`${this.props.match.path}/create`} component={CategoryCreate} />
+                    <Redirect to={{ pathname: `/${getLangPrefix(this.props.match.path)}/404` }} />
+                </Switch>
             </div>
         );
     }
---
+++ src/components/admin/categories/create/CategoryCreate.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route, Redirect, RouteComponentProps } from "react-router-dom";
+
+
+class CategoryCreate extends React.PureComponent<any> {
+
+    render() {
+        return (
+            <div>
+                <h1>Category Create</h1>
+            
+            </div>
+        );
+    }
+}
+
+export default CategoryCreate;
