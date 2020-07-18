a643d35d517265464480708b279a8857d996239e
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-18 12:07:47 +0300

    admin component switching

--- src/components/admin/AdminComponent.tsx
+++ src/components/admin/AdminComponent.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Switch, Route, Redirect, RouteComponentProps } from "react-router-dom";
 import { withTranslation, Trans, WithTranslation } from 'react-i18next';
+import CategoriesComponent from "./categories/CategoriesComponent";
+import ProjectsComponent from "./projects/ProjectsComponent";
+import { getLangPrefix } from "../../util/LangPrefixUtil";
 
-class AdminComponent extends React.PureComponent<WithTranslation> {
+class AdminComponent extends React.PureComponent<WithTranslation & RouteComponentProps> {
 
     render() {
         return (
@@ -10,6 +14,12 @@ class AdminComponent extends React.PureComponent<WithTranslation> {
                 <Trans i18nKey="navbar:content.text">
                     Welcome at <strong>our place</strong>.
                 </Trans>
+
+                <Switch>
+                    <Route path={`${this.props.match.path}/category`} component={CategoriesComponent} />
+                    <Route path={`${this.props.match.path}/projects`} component={ProjectsComponent} />
+                    <Redirect to={{ pathname: `/${getLangPrefix(this.props.match.path)}/404` }} />
+                </Switch>
             </div>
         );
     }
---
+++ src/components/admin/categories/CategoriesComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class CategoriesComponent extends React.PureComponent<any> {
+
+    render() {
+        return (
+            <div>
+                <h1>CategoriesComponent</h1>
+            
+            </div>
+        );
+    }
+}
+
+export default CategoriesComponent;
