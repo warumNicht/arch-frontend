3e6c448acac1c705cb258fbfb70f33d7bab4aa53
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-18 15:05:41 +0300

    form and select

--- src/components/admin/categories/create/CategoryCreate.tsx
+++ src/components/admin/categories/create/CategoryCreate.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import { Switch, Route, Redirect, RouteComponentProps } from "react-router-dom";
+import { LangEnum } from "../../../../constants/appConstants";
+import { languagesArray } from '../../../../constants/appConstants';
+
+const createLangOptions = () => {
+    return (
+        languagesArray.map((lang: string) => {
+            return <option key={lang} value={lang}>{lang}</option>
+        })
+    )
+}
 
 
 class CategoryCreate extends React.PureComponent<any> {
+
+    handleSubmit = (event: any) => {
+        event.preventDefault();
+        console.log(event)
+    }
+
+    handleChange = (event: any) => {
+        event.preventDefault();
+        console.log(event)
+    }
 
     render() {
         return (
             <div>
                 <h1>Category Create</h1>
-            
+
+                <form onSubmit={this.handleSubmit}>
+                    <select value={languagesArray[1]} onChange={this.handleChange}>
+                        {createLangOptions()}
+                    </select>
+                </form>
+
+
             </div>
         );
     }
