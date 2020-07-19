727998fa7c7b2cb54554acab4949799d0d1b7c28
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-19 19:23:14 +0300

    rem import

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import backendHost from '../constants/appConstants';
 
 const api = axios.create({
