aaf171ced49b003bca32ce4e479aa411c2f41876
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-22 15:53:58 +0300

    category edit component

---
+++ src/components/admin/categories/edit/CategoryEdit.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class CategoryEdit extends React.PureComponent<any> {
+
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <h1>Category Edit</h1>
+            
+            </div>
+        );
+    }
+}
+
+export default CategoryEdit;
