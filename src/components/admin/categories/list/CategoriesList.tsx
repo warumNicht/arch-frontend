4862b52b9d266b4a4bdd08c61472af09673f9a3e
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-22 15:33:48 +0300

    from class to const

--- src/components/admin/categories/list/CategoriesList.tsx
+++ src/components/admin/categories/list/CategoriesList.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import ArchitectureAppStore from "../../../../redux/interfaces/ArchitectureAppStore";
+import { Link } from "react-router-dom";
+import ArchitectureAppStore, { Category } from "../../../../redux/interfaces/ArchitectureAppStore";
 
+const createCategoriesLinks = (categories: Category[]) => {
+    return (
+        categories.map((category: Category) => {
+            return <Link to={`/admin/category/edit/${category.id}`}>{category.name}</Link>
+        })
+    )
+}
 
+interface CategoryListProps {
+    categories: Category[]
+}
 
-class CategoriesList extends React.PureComponent<any> {
+const CategoriesList = (props: CategoryListProps) => {
 
+    return (
+        <div>
+            <h1>Categories List</h1>
+            <div>{createCategoriesLinks(props.categories)}</div>
+        </div>
+    );
 
-    render() {
-        console.log(this.props)
-        return (
-            <div>
-                <h1>Categories List</h1>
-            
-            </div>
-        );
-    }
 }
 const mapStateToProps = (state: ArchitectureAppStore) => ({
     categories: state.categories
-  });
-  
+});
+
 export default connect(mapStateToProps)(CategoriesList);
