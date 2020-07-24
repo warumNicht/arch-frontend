28bbd34b5422d5e0e16186136aa052dbd008d81a
Andrey Minev <andrey_st_minev@yahoo.com>
2020-07-24 09:41:18 +0300

    valid messages style

--- src/shared/ValidationMessages/ValidationMessages.tsx
+++ src/shared/ValidationMessages/ValidationMessages.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './ValidationMessages.css';
 
 const listMessages = (messages: string[]) => {
     return (
@@ -14,7 +15,7 @@ class ValidationMessages extends React.PureComponent<any> {
     render() {
         console.log(this.props.messages)
         return (
-            <div>
+            <div className='validation-messages-holder'>
                 {listMessages(this.props.messages)}
             </div>
         );
